94841aeaacc87a6af17d59ae8afa31b76422cc3d
masadi <masadi.com@gmail.com>
2022-07-07 07:43:23 +0700

    update

---
+++ app/Console/Commands/GenerateUser.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+use App\Models\Role;
+
+class GenerateUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'generate:user';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        User::truncate();
+        $user = User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@smkariyametta.sch.id',
+            'password' => bcrypt('12345678'),
+        ]);
+        $admin = Role::where('name', 'administrator')->first();
+        $user->attachRole($admin);
+    }
+}
